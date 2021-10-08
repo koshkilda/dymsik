bc95fe1e6b1a023af882eb7c58756c08b0e282a3
koshkilda <89313188+koshkilda@users.noreply.github.com>
2021-10-08 20:19:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
 #include <stddef.h>
 #include "equation.h"
-#include "tests.h"
 #include "helpfunction.h"
 
 
 int main() {
 
-   // void TestSum();
+    printf("Solve quadratic equation\n"
+        "2021\n\n");
 
-   printf("Solve quadratic equation\n"
-       "2021\n\n");
+    int finish_or_not = 1;
 
-   int finish_or_not = 1;
+    while (finish_or_not != 0) {
 
-   while ( finish_or_not != 0) {
+        printf("Enter a, b, c: ");
 
-      printf("Enter a, b, c: ");
+        double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+        input(&a, &b, &c);
 
-      double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-      input(&a, &b, &c);
+        quadratic_solve(a, b, c, x1, x2);
 
-      quadratic_solve(a, b, c, &x1, &x2);
-
-      printf("Enter 0 if you want to finish\n");
-      scanf("%d", &finish_or_not);
-   }
-   return 0;
+        printf("Enter 0 if you want to finish\n");
+        scanf("%d", &finish_or_not);
+    }
+    return 0;
 }
