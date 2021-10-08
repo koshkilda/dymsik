e70237289e3e7992c2e1934d9a5f27d5b1b3b43d
koshkilda <89313188+koshkilda@users.noreply.github.com>
2021-10-08 20:16:41 +0300

    Update equation.h

--- equation.h
+++ equation.h
@@ -1,7 +1,3 @@
 #pragma once
 
-#include "equation.c"
-
-double real_sum_Roots(double a, double b, double c);
-
-int quadratic_solve(double a, double b, double c, double* x1, double* x2);
+void quadratic_solve(double a, double b, double c, double x1, double x2);
