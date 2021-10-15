15539bfa87923a890089351d0f42a8ea72ffbd78
koshkilda <89313188+koshkilda@users.noreply.github.com>
2021-10-15 20:50:09 +0300

    Update equation.c

--- equation.c
+++ equation.c
@@ -8,12 +8,13 @@
 #define CONST 0.001
 
 
-void linear_equation(double k, double b) {
+int linear_equation(double k, double b, double* x1) {
     if (is_equal(fabs(k), 0) == 0) {
-        printf("x = %lf\n", -b / k);
+        *x1 = -b / k;
+        return 1;
     }
     else {
-        printf("try again\n");
+        return -1;
     }
 }
 
@@ -37,30 +38,19 @@ int unlinear_equation(double a, double b, double c, double* x1, double* x2) {
     }
 }
 
-void quadratic_solve(double a, double b, double c, double x1, double x2) {
+int square_solver(double a, double b, double c, double x1, double x2) {
 
-    int nRoots;
+    int nRoots = -5;
 
     if (is_equal(fabs(a), 0) == 1) {
-        linear_equation(b, c);
+        nRoots = linear_equation(b, c, &x1);
     }
 
     else {
         nRoots = unlinear_equation(a, b, c, &x1, &x2);
-        switch (nRoots) {
-        case 0:
-            printf("no roots\n");
-            break;
-        case 1:
-            printf("x = %lf\n", x1);
-            break;
-        case 2:
-            printf("x1 = %.2lf x2 = %.2lf\n\n", x1, x2);
-            break;
-        default:
-            printf("number of Roots = %d \n\n", nRoots);
-            break;
-        }
     }
+
+    output(nRoots, x1, x2);
+    return nRoots;
 
 }
